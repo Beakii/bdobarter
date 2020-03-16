fb09b8ac1724279ee4518b13812b1b533b5d6d6e
James <auminist3@gmail.com>
2020-03-16 14:57:54 +1200

    Updates

---
+++ precache-manifest.10c72659421799364cdc0c99348be782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78548a868e99b75f441df3c51c62b87e",
+    "url": "/bdobarter/index.html"
+  },
+  {
+    "revision": "9a7402caaa958b4c410c",
+    "url": "/bdobarter/static/css/main.db34fd91.chunk.css"
+  },
+  {
+    "revision": "faf2c2b168eb1d306903",
+    "url": "/bdobarter/static/js/2.e5d28eab.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bdobarter/static/js/2.e5d28eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7402caaa958b4c410c",
+    "url": "/bdobarter/static/js/main.19ddcc6f.chunk.js"
+  },
+  {
+    "revision": "9fb026be8786d940cd11",
+    "url": "/bdobarter/static/js/runtime-main.7c83c81d.js"
+  }
+]);
